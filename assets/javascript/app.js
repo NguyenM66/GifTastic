cfc21df4f73590a11dd8e06a68192f12b6f31507
Monica <monica.nguyen@gmail.com>
2017-08-23 00:40:48 -0400

    merging missing stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// start with set of buttons already to go
+//generate dynamic html buttons
+
+// with an add animal form on the side
+//make html search 
+
+// minimal html
+// js populate buttons
+
+// floats images left
+// pulls up 10 images
+
+// click to play click again to pause
+// rating of gif at the top of each gif
+
+// //array with animals
+
+// //button handler
+// funtion to generate buttons
+// funtion to add buttons 
+
+
+// //gif handler
+// funtion to populate html for gif and its rating
+// function to pause gif
+// funtion to play gif
